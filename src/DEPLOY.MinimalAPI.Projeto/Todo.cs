40e15df4410a75e3a70df5143036f81453504355
Felipe Augusto <felipe.augusto@avanade.com>
2024-04-24 09:53:59 -0300

    feat: initial commit

---
+++ src/DEPLOY.MinimalAPI.Projeto/Todo.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+partial class TodoDb : DbContext
+{
+    public TodoDb(DbContextOptions<TodoDb> options)
+    : base(options) { }
+
+    public DbSet<Todo> Todos => Set<Todo>();
+}
+
+public class Todo
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public bool IsComplete { get; set; }
+}
